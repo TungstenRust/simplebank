1c8e676152b4aff3ab7b01ea5637336b3cebfe29
Tungsten Rust <tungsten.rust@gmail.com>
2024-01-08 03:24:25 -0800

    main.go draft version of server instance is ready postman request tests should be done next

--- main.go
+++ main.go
@@ -1,2 +1,29 @@
-package simplebank
+package main
 
+import (
+	"database/sql"
+	"github.com/TungstenRust/simplebank/api"
+	db "github.com/TungstenRust/simplebank/db/sqlc"
+	_ "github.com/lib/pq"
+	"log"
+)
+
+const (
+	dbDriver      = "postgres"
+	dbSource      = "postgresql://root:secret@localhost:5432/simple_bank?sslmode=disable"
+	serverAddress = "0.0.0.0:8080"
+)
+
+func main() {
+	conn, err := sql.Open(dbDriver, dbSource)
+	if err != nil {
+		log.Fatal("can't connect to the database;", err)
+	}
+
+	store := db.NewStore(conn)
+	server := api.NewServer(store)
+	err = server.Start(serverAddress)
+	if err != nil {
+		log.Fatal("cannot start server:", err)
+	}
+}
