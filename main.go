60420894f0184aa6e6ed4955bcc556947512a0be
Tungsten Rust <tungsten.rust@gmail.com>
2024-01-23 14:33:44 -0800

    protobuf is created

--- main.go
+++ main.go
@@ -4,9 +4,14 @@ import (
 	"database/sql"
 	"github.com/TungstenRust/simplebank/api"
 	db "github.com/TungstenRust/simplebank/db/sqlc"
+	"github.com/TungstenRust/simplebank/gapi"
+	"github.com/TungstenRust/simplebank/pb"
 	"github.com/TungstenRust/simplebank/util"
 	_ "github.com/lib/pq"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
 	"log"
+	"net"
 )
 
 func main() {
@@ -18,13 +23,35 @@ func main() {
 	if err != nil {
 		log.Fatal("can't connect to the database;", err)
 	}
-
 	store := db.NewStore(conn)
+	runGrpcServer(config, store)
+}
+
+func runGrpcServer(config util.Config, store db.Store) {
+	server, err := gapi.NewServer(config, store)
+	if err != nil {
+		log.Fatal("cannot start server: ", err)
+	}
+	grpcServer := grpc.NewServer()
+	pb.RegisterSimpleBankServer(grpcServer, server)
+	reflection.Register(grpcServer)
+	listener, err := net.Listen("tcp", config.GRPCServerAddress)
+	if err != nil {
+		log.Fatal("cannot create listener")
+	}
+	log.Printf("start gRPC server at %s", listener.Addr().String())
+	err = grpcServer.Serve(listener)
+	if err != nil {
+		log.Fatal("cannot start gRPC server")
+	}
+}
+
+func runGinServer(config util.Config, store db.Store) {
 	server, err := api.NewServer(config, store)
 	if err != nil {
 		log.Fatal("cannot start server: ", err)
 	}
-	err = server.Start(config.ServerAddress)
+	err = server.Start(config.HTTPServerAddress)
 	if err != nil {
 		log.Fatal("cannot start server:", err)
 	}
