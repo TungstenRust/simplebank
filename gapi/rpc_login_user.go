772e68550c7c5d1898ce9530087eb9b41823ff76
Tungsten Rust <tungsten.rust@gmail.com>
2024-01-23 16:58:58 -0800

    gprc create user and login user is created

---
+++ gapi/rpc_login_user.go
@@ -0,0 +1,53 @@
+package gapi
+
+import (
+	"database/sql"
+	db "github.com/TungstenRust/simplebank/db/sqlc"
+	"github.com/TungstenRust/simplebank/pb"
+	"github.com/TungstenRust/simplebank/util"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+)
+
+func (server *Server) LoginUser(ctx context.Context, req *pb.LoginUserRequest) (*pb.LoginUserResponse, error) {
+	user, err := server.store.GetUser(ctx, req.Username)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			return nil, status.Errorf(codes.NotFound, "user not found")
+		}
+		return nil, status.Errorf(codes.Internal, "failed to find user")
+	}
+	err = util.CheckPassword(req.Password, user.HashedPassword)
+	if err != nil {
+		return nil, status.Errorf(codes.NotFound, "incorrect password")
+	}
+	accessToken, accessPayload, err := server.tokenMaker.CreateToken(
+		user.Username,
+		server.config.AccessTokenDuration,
+	)
+	if err != nil {
+		return nil, status.Errorf(codes.Internal, "failed to failed to create access token")
+	}
+	refreshToken, refreshPayload, err := server.tokenMaker.CreateToken(
+		user.Username,
+		server.config.RefreshTokenDuration,
+	)
+	if err != nil {
+		return nil, status.Errorf(codes.Internal, "failed to failed to create refresh token")
+	}
+	//ctx.ContentType() Shubhamdan sorush mutleq
+	session, err := server.store.CreateSession(ctx, db.CreateSessionParams{
+		ID:           refreshPayload.ID,
+		Username:     user.Username,
+		RefreshToken: refreshToken,
+		UserAgent:    "",
+		ClientIp:     "",
+		IsBlocked:    false,
+		ExpiresAt:    refreshPayload.ExpiredAt,
+	})
+	if err != nil {
+		return nil, status.Errorf(codes.Internal, "failed to failed to create session")
+	}
+	rsp := &pb.LoginUserResponse{}
+	return rsp, nil
+}
