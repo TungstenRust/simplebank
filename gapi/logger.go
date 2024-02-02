bde3d48ac155a41e9e922f2c20f47c5911d35351
Tungsten Rust <tungsten.rust@gmail.com>
2024-02-02 00:28:44 -0800

    logger is added

--- gapi/logger.go
+++ gapi/logger.go
@@ -23,7 +23,11 @@ func GrpcLogger(
 	if st, ok := status.FromError(err); ok {
 		statusCode = st.Code()
 	}
-	log.Info().Str("protocol", "grpc").
+	logger := log.Info()
+	if err != nil {
+		logger = log.Error().Err(err)
+	}
+	logger.Str("protocol", "grpc").
 		Str("method", info.FullMethod).
 		Int("status_code", int(statusCode)).
 		Str("status_text", statusCode.String()).
