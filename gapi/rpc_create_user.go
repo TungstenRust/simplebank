0962a483d86d96289663ee3c7adf359bf9e47958
Tungsten Rust <tungsten.rust@gmail.com>
2024-02-06 09:12:40 -0800

    asynq continues again

--- gapi/rpc_create_user.go
+++ gapi/rpc_create_user.go
@@ -46,7 +46,7 @@ func (server *Server) CreateUser(ctx context.Context, req *pb.CreateUserRequest)
 	opts := []asynq.Option{
 		asynq.MaxRetry(10),
 		asynq.ProcessIn(10 * time.Second),
-		asynq.Queue("critical"),
+		asynq.Queue(worker.QueueCritical),
 	}
 	err = server.taskDistributor.DistributeTaskSendVerifyEmail(ctx, taskPayload, opts...)
 	if err != nil {
