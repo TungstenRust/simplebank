d661c390852782abe2223bd31c2b19c1a5c7d18a
Tungsten Rust <tungsten.rust@gmail.com>
2024-02-06 12:15:39 -0800

    tx_create_user and tx_transfer finished

--- db/sqlc/tx_create_user.go
+++ db/sqlc/tx_create_user.go
@@ -6,10 +6,13 @@ import (
 
 // TransferTxParams  contains the input parameters of the transfer transaction
 type CreateUserTxParams struct {
+	CreateUserParams
+	AfterCreate func(user User) error
 }
 
 // TransferTxResult is the result of the transfer transaction
 type CreateUserTxResult struct {
+	User User
 }
 
 //CreateUserTx performs a money transfer from one account to the another account.
@@ -19,8 +22,11 @@ func (store *SQLStore) CreateUserTx(ctx context.Context, arg CreateUserTxParams)
 	var result CreateUserTxResult
 	err := store.execTX(ctx, func(q *Queries) error {
 		var err error
-
-		return err
+		result.User, err = q.CreateUser(ctx, arg.CreateUserParams)
+		if err != nil {
+			return err
+		}
+		return arg.AfterCreate(result.User)
 	})
 	return result, err
 }
