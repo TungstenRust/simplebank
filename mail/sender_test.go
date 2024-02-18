f0bf2b529317656b90f24f3638159f3d8f7ce0ba
Tungsten Rust <tungsten.rust@gmail.com>
2024-02-18 04:28:27 -0800

    notification sender addition is finished

--- mail/sender_test.go
+++ mail/sender_test.go
@@ -1,1 +1,22 @@
 package mail
+
+import (
+	"github.com/TungstenRust/simplebank/util"
+	"github.com/stretchr/testify/require"
+	"testing"
+)
+
+func TestSendEmailWithGmail(t *testing.T) {
+	config, err := util.LoadConfig("..")
+	require.NoError(t, err)
+	sender := NewGmailSender(config.EmailSenderName, config.EmailSenderAddress, config.EmailSenderPassword)
+	subject := "A test email"
+	content := `
+	<h1>Hello world</h1>
+	<p>This is a test message from <a href="https://simplebank.biz">Shagen is Learning</a></p>
+	`
+	to := []string{"simplebankbiz@gmail.com"}
+	attachFiles := []string{"../README.md"}
+	err = sender.SendEmail(subject, content, to, nil, nil, attachFiles)
+	require.NoError(t, err)
+}
