0ea6ce917641a9190b9908564cfd9f05e2dea8d5
Tungsten Rust <tungsten.rust@gmail.com>
2024-03-12 05:06:03 -0700

    RBAC addition is started

--- doc/schema.sql
+++ doc/schema.sql
@@ -1,9 +1,10 @@
 -- SQL dump generated using DBML (dbml-lang.org)
 -- Database: PostgreSQL
--- Generated at: 2024-02-18T12:45:09.134Z
+-- Generated at: 2024-03-12T12:05:04.568Z
 
 CREATE TABLE "users" (
   "username" varchar PRIMARY KEY,
+  "role" varchar NOT NULL DEFAULT 'depositor',
   "hashed_password" varchar NOT NULL,
   "full_name" varchar NOT NULL,
   "email" varchar UNIQUE NOT NULL,
